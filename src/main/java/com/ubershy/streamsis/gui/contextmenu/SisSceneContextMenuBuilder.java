f99eeba72ce474dd2382d111074e31314cdbaa9d
Eva Balycheva <ubershy@gmail.com>
2016-10-31 16:15:00 +0300

    Improve context menu for SisScene
    This patch makes it possible to add new SisScene by right clicking on
    one of SisScenes. This helps when list is so full that it has no free
    space.
    This patch also makes StreamSis to ask confirmation before deleting
    SisScene with Actors.

--- src/main/java/com/ubershy/streamsis/gui/contextmenu/SisSceneContextMenuBuilder.java
+++ src/main/java/com/ubershy/streamsis/gui/contextmenu/SisSceneContextMenuBuilder.java
@@ -17,6 +17,12 @@
  */
 package com.ubershy.streamsis.gui.contextmenu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
 import com.ubershy.streamsis.gui.GUIManager;
 import com.ubershy.streamsis.gui.helperclasses.GUIUtil;
 import com.ubershy.streamsis.project.CuteProject;
@@ -24,6 +30,9 @@ import com.ubershy.streamsis.project.SisScene;
 import com.ubershy.streamsis.project.ProjectManager;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.ListView;
@@ -35,23 +44,27 @@ public class SisSceneContextMenuBuilder {
 		CustomMenuItem addSisSceneMenuItem = GUIUtil.createTooltipedMenuItem("Add new",
 				"Add new SisScene in which you can add Actors later.");
 		addSisSceneMenuItem.setOnAction((ActionEvent event) -> {
-			String genericName = "New SisScene";
-			String alteredName = genericName;
-			int counter = 0;
-			while (ProjectManager.getProject().getSisSceneByName(alteredName) != null) {
-				counter++;
-				alteredName = String.format("%s(%d)", genericName, counter);
-			}
-			SisScene newSisScene = new SisScene(alteredName, new String[] {});
-			// Initialize SisScene to highlight that it is still not configured
-			newSisScene.init();
-			ProjectManager.getProject().addSisScene(newSisScene);
-			// Initialize whole project
-			ProjectManager.getProject().init();
+			addSisScene();
 		});
 		cm.getItems().add(addSisSceneMenuItem);
 		cm.autoHideProperty().set(true);
 		return cm;
+	}
+	
+	private static void addSisScene() {
+		String genericName = "New SisScene";
+		String alteredName = genericName;
+		int counter = 0;
+		while (ProjectManager.getProject().getSisSceneByName(alteredName) != null) {
+			counter++;
+			alteredName = String.format("%s(%d)", genericName, counter);
+		}
+		SisScene newSisScene = new SisScene(alteredName, new String[] {});
+		// Initialize SisScene to highlight that it is still not configured
+		newSisScene.init();
+		ProjectManager.getProject().addSisScene(newSisScene);
+		// Initialize whole project
+		ProjectManager.getProject().init();
 	}
 
 	public static ContextMenu createSisSceneItemContextMenu(PossibleMoves possibleMoves) {
@@ -61,14 +74,24 @@ public class SisSceneContextMenuBuilder {
 		CustomMenuItem setPrimarySisSceneMenuItem = GUIUtil.createTooltipedMenuItem(
 				"Set as Primary SisScene", "Primary SisScene starts first when you start Project.");
 		CustomMenuItem deleteSisSceneMenuItem = GUIUtil.createTooltipedMenuItem("Delete",
-				"Delete the SisScene. Actors that were inside of it will be still accessible for reuse.");
+				"Delete the SisScene."
+				+ " Actors that were inside of it will be still accessible for reuse.");
 		CustomMenuItem deleteSisSceneWithActorsMenuItem = GUIUtil.createTooltipedMenuItem(
 				"Delete with Actors",
-				"Delete the SisScene and Actors.\nActors will be deleted globally from this and all others SisScenes without recovery.");
+				"Delete the SisScene and Actors."
+				+ "\nActors will be deleted globally from this and all others SisScenes without "
+				+ "recovery.");
 		CustomMenuItem moveUpSisSceneMenuItem = GUIUtil.createTooltipedMenuItem("Move Up",
-				"The order of SisScenes doesn't matter.\nBut you can still move them around if you want. ;)");
+				"The order of SisScenes doesn't matter."
+				+ "\nBut you can still move them around if you want. ;)");
 		CustomMenuItem moveDownSisSceneMenuItem = GUIUtil.createTooltipedMenuItem("Move Down",
-				"The order of SisScenes doesn't matter.\nBut you can still move them around if you want. ;)");
+				"The order of SisScenes doesn't matter."
+				+ "\nBut you can still move them around if you want. ;)");
+		CustomMenuItem addSisSceneMenuItem = GUIUtil.createTooltipedMenuItem(
+				"Add new SisScene to the list", "Add new SisScene to the end of the list.");
+		addSisSceneMenuItem.setOnAction((ActionEvent event) -> {
+			addSisScene();
+		});
 		deleteSisSceneMenuItem.setOnAction((ActionEvent event) -> {
 			SisScene scene = sisSceneList.getSelectionModel().getSelectedItem();
 			project.removeSisScene(scene);
@@ -77,9 +100,43 @@ public class SisSceneContextMenuBuilder {
 		});
 		deleteSisSceneWithActorsMenuItem.setOnAction((ActionEvent event) -> {
 			SisScene scene = sisSceneList.getSelectionModel().getSelectedItem();
-			project.removeSisSceneWithActors(scene);
-			// Initialize whole project
-			ProjectManager.getProject().init();
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Delete SisScene with Actors");
+			alert.setHeaderText("This SisScene will be deleted."
+					+ "\nAlso the Actors of this SisScene will be deleted globally and from all"
+					+ " other SisScenes without recovery.");
+			String poorActors = "";
+			Random random = new Random();
+			List<String> phrases = new ArrayList<String>(Arrays.asList(
+					"I have a wife and kids", "I still have some tricks up my sleeve ;)",
+					"Please no...", "You can't delete me just like that!",
+					"Treat me bad, I need the reputation", "Maybe... we can make a deal?",
+					"Don't delete me, I promise I will perform better next time!",
+					"I don't care anymore...", "My Actions were that bad?",
+					"If you will delete me, I will have no choice but to become a streamer..."));
+			for (String actorName : scene.getActorNames()) {
+				poorActors += "\t" + actorName;
+				if (random.nextInt(7) == 0) {
+					if (phrases.size() != 0) {
+						int phraseIndex = random.nextInt(phrases.size());
+						poorActors += "\t\t- \"" + phrases.get(phraseIndex) + "\"";
+						phrases.remove(phraseIndex);
+					}
+				}
+				poorActors += "\n";
+			}
+			String warning = "";
+			if (!poorActors.isEmpty()) {
+				warning = "Take a good look of these poor Actors:\n\n" + poorActors;
+			}
+			alert.setContentText(warning + "\n" + "Is it okay for you to delete them?");
+			GUIUtil.cutifyAlert(alert);
+			Optional<ButtonType> result = alert.showAndWait();
+			if (result.get() == ButtonType.OK) {
+				project.removeSisSceneWithActors(scene);
+				// Initialize whole project
+				ProjectManager.getProject().init();
+			}
 		});
 		setPrimarySisSceneMenuItem.setOnAction((ActionEvent event) -> {
 			project.setPrimarySisSceneName(
@@ -91,22 +148,23 @@ public class SisSceneContextMenuBuilder {
 		moveDownSisSceneMenuItem.setOnAction((ActionEvent event) -> {
 			project.moveDownSisScene(sisSceneList.getSelectionModel().getSelectedItem());
 		});
+		cm.getItems().addAll(moveUpSisSceneMenuItem, moveDownSisSceneMenuItem,
+				setPrimarySisSceneMenuItem, deleteSisSceneMenuItem,
+				deleteSisSceneWithActorsMenuItem, addSisSceneMenuItem);
 		switch (possibleMoves) {
 		case NOWHERE:
+			cm.getItems().clear();
+			cm.getItems().add(addSisSceneMenuItem);
 			break;
 		case ONLYDOWN:
-			cm.getItems().add(moveDownSisSceneMenuItem);
+			cm.getItems().remove(moveUpSisSceneMenuItem);
 			break;
 		case ONLYUP:
-			cm.getItems().add(moveUpSisSceneMenuItem);
+			cm.getItems().remove(moveDownSisSceneMenuItem);
 			break;
 		case UPORDOWN:
-			cm.getItems().add(moveUpSisSceneMenuItem);
-			cm.getItems().add(moveDownSisSceneMenuItem);
 			break;
 		}
-		cm.getItems().addAll(setPrimarySisSceneMenuItem, deleteSisSceneWithActorsMenuItem,
-				deleteSisSceneMenuItem);
 		cm.autoHideProperty().set(true);
 		return cm;
 	}
