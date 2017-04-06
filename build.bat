39b9befdd100ea66d2d11a04903f4aa4452abe42
Eva Balycheva <ubershy@gmail.com>
2017-04-06 06:30:23 +0300

    Change the way StreamSis is assembled
    Before this patch StreamSis was assembled into stand-alone
    application with appassembler maven plugin.
    
    This patch makes StreamSis project build itself via maven in more
    traditional, fully configurable way.
    
    Also it makes use of launch4j maven plugin for generating Windows
    executable of StreamSis.

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
-CALL mvn clean package appassembler:assemble
+CALL mvn clean package
 @ECHO off
 ECHO.
 IF %ERRORLEVEL% == 0 (
 	ECHO Yay. Successfully finished assembling StreamSis.
-	ECHO You can check the result in 'target/appassembler' subdirectory now.
+	ECHO You can check the result in 'target/assembled' subdirectory now.
 )
 IF NOT %ERRORLEVEL% == 0 (
-	ECHO StreamSis assembly fail. See log.
+	ECHO StreamSis assembly has failed. See the log.
 )
 ECHO.
 PAUSE
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean package appassembler:assemble
+mvn clean package
